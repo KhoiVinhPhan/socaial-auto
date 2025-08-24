9b87db2f0a662942f8f31940943f3688f7274692
khoivinhphan <khoivinhphan@gmail.com>
2025-08-24 23:33:31 +0700

    fix

--- data/data_device.py
+++ data/data_device.py
@@ -1,8 +1,8 @@
 # ==== CẤU HÌNH THIẾT BỊ ====
 array = [
     {"serial": "127.0.0.1:5745", "window_title": "Vinh1", "resolution": (427, 735)},
-    # {"serial": "127.0.0.1:5655", "window_title": "Vinh2", "resolution": (427, 735)},
-    # {"serial": "127.0.0.1:5695", "window_title": "Vinh3", "resolution": (427, 735)},
+    {"serial": "127.0.0.1:5755", "window_title": "Vinh2", "resolution": (427, 735)},
+    {"serial": "127.0.0.1:5765", "window_title": "Vinh3", "resolution": (427, 735)},
     # {"serial": "127.0.0.1:5705", "window_title": "Vinh4", "resolution": (427, 735)},
     # {"serial": "127.0.0.1:5715", "window_title": "Vinh5", "resolution": (427, 735)},
     {"serial": "127.0.0.1:5735", "window_title": "Vinh6", "resolution": (427, 735)},
